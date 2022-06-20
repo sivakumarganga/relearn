c183f94548ff99eb4ec3dc9ce5666fd76cb7cc45
Sivakumar Siddam <sivakumarganga@gmail.com>
2022-06-20 21:32:24 +0530

    Selection sort changes

---
+++ src/algo/sort/ReLearn.Alog.Sort/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReLearn.Alog.Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectionSort.Sort(new int[] { 64, 25, 12, 22, 11 });
+            Console.Read();
+        }
+    }
+}
---
+++ src/algo/sort/ReLearn.Alog.Sort/SelectionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReLearn.Alog.Sort
+{
+    public class SelectionSort
+    { 
+        public static void Sort(int[] dataStream)
+        {
+            PrintArray(dataStream,"Before Sorting");
+            for (int i = 0; i < dataStream.Length; i++)
+            {
+                int shortIndex = i;
+                for (int j = i + 1; j < dataStream.Length; j++)
+                {
+                    if( dataStream[j]< dataStream[shortIndex])
+                    {
+                        shortIndex = j;
+                    }
+                }
+                int temp = dataStream[i];
+                dataStream[i] = dataStream[shortIndex];
+                dataStream[shortIndex] = temp;
+            }
+            PrintArray(dataStream,"After Sorting");
+           
+        }
+        public static void PrintArray(int[] dataStream,string message)
+        {
+            Console.WriteLine(message);
+            for (int i = 0; i < dataStream.Length; i++)
+                Console.Write($"{dataStream[i]} ");
+            Console.WriteLine("");
+        }
+    }
+}
