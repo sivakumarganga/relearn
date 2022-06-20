8d8f86265315b457d5ad0a472478526d1b9ed4f3
Sivakumar Siddam <sivakumarganga@gmail.com>
2022-06-20 20:38:05 +0530

    binary search changes to handle not found item

---
+++ src/algo/search/ReLearn.Algo.Search/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReLearn.Algo.Search
+{
+    public static class BinarySearch
+    {
+        public static void Search(int[] dataStream,int number)
+        {
+            int tail=0;
+            int head = dataStream.Length - 1;
+            int mid = -1;
+            int iterations=0;
+            while (tail!=head&&tail < dataStream.Length && head < dataStream.Length)
+            {
+                 mid = (tail + head) / 2;
+                if(dataStream[mid] == number)
+                {
+                    break;
+                }
+                else if(dataStream[mid] <number)
+                {
+                    tail = mid + 1;
+                }else
+                {
+                    head = mid;
+                }
+                mid = -1;
+                iterations++;
+            }
+            Console.WriteLine($"Found Value:{number} at {mid} with Iterations:{iterations}");
+        }
+    }
+}
---
+++ src/algo/search/ReLearn.Algo.Search/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReLearn.Algo.Search
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(int i in new int[] {0,1,30,51,100,101 })
+            {
+                BinarySearch.Search(new int[] { 1, 8, 10, 23, 30, 51, 56, 74, 93, 100 }, i);
+            }
+           
+            Console.Read();
+        }
+    }
+}
