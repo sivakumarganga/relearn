c7a6783a5149183e46d589570a0bec1ff6bfb170
Sivakumar Siddam <sivakumarganga@gmail.com>
2022-06-22 10:23:21 +0530

    quick sort changes

--- src/algo/sort/ReLearn.Alog.Sort/Program.cs
+++ src/algo/sort/ReLearn.Alog.Sort/Program.cs
@@ -6,8 +6,9 @@ namespace ReLearn.Alog.Sort
     {
         static void Main(string[] args)
         {
-            SelectionSort.Sort(new int[] { 64, 25, 12, 22, 11 });
-            InsertionSort.Sort(new int[] { 9, 4, 5, 1, 3, 2 });
+            //SelectionSort.Sort(new int[] { 64, 25, 12, 22, 11 });
+            //InsertionSort.Sort(new int[] { 9, 4, 5, 1, 3, 2 });
+           QuickSort.Sort(new int[] { 9, 4, 5, 1, 3, 2 });
             Console.Read();
         }
     }
---
+++ src/algo/sort/ReLearn.Alog.Sort/QuickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReLearn.Alog.Sort
+{
+    public class QuickSort
+    {
+        public static void Sort(int[] dataStream)
+        {
+            PrintArray(dataStream, "Quick Sort: Before");
+            QuickSortAlgo(dataStream, 0, dataStream.Length - 1);
+            PrintArray(dataStream, "Quick Sort: After");
+        }
+        public static void QuickSortAlgo(int[] dataStream, int low, int high)
+        {
+            if (low >= high)
+                return;
+            int pivot = QuickPartition(dataStream, low, high);
+            QuickSortAlgo(dataStream, low, pivot - 1);
+            QuickSortAlgo(dataStream, pivot + 1, high);
+
+        }
+        public static int QuickPartition(int[] dataStream,int low,int high)
+        {
+            int pivot = dataStream[high];
+            int pointerToLowerItem = low-1;
+            for (int i = low; i < high; i++)
+            {
+                //Check if array item is less then the pivot 
+                if (dataStream[i] < pivot)
+                {
+                    //Then Swap with last pointer
+                    pointerToLowerItem++;
+                    int temp = dataStream[pointerToLowerItem];
+                    dataStream[pointerToLowerItem] = dataStream[i];
+                    dataStream[i] = temp;
+                }
+            }
+            //At last swap pivot (high indexed) value to center
+            dataStream[high] = dataStream[++pointerToLowerItem];
+            dataStream[pointerToLowerItem] = pivot;
+            return pointerToLowerItem;
+        }
+
+        public static void PrintArray(int[] dataStream, string message)
+        {
+            Console.WriteLine(message);
+            for (int i = 0; i < dataStream.Length; i++)
+                Console.Write($"{dataStream[i]} ");
+            Console.WriteLine("");
+        }
+    }
+}
