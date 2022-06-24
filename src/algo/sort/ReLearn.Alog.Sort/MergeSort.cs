3f88e08c09d9eb8b20ee73be479940b3e39a6490
Sivakumar Siddam <sivakumarganga@gmail.com>
2022-06-24 10:41:09 +0530

    merge sort logic

---
+++ src/algo/sort/ReLearn.Alog.Sort/MergeSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReLearn.Alog.Sort
+{
+    public class MergeSort
+    {
+        public static void Sort(int[] dataStream)
+        {
+            PrintArray(dataStream, "MergeSort Sort: Before");
+            DevideNMerge(dataStream, 0, dataStream.Length - 1);
+            PrintArray(dataStream, "MergeSort Sort: After");
+        }
+        public static void DevideNMerge(int[] dataStream,int left,int right)
+        {
+            if (left >= right)
+                return;
+
+            int m = left + (right - left) / 2;
+            DevideNMerge(dataStream, left, m);
+            DevideNMerge(dataStream, m+1, right);
+            Merge(dataStream, left, right, m);
+        }
+        public static void Merge(int[] dataStream, int left, int right, int middle)
+        {
+            int leftArrarySize = middle - left +1;
+            int rightArrarySize = right - middle;
+
+            int[] leftArray = new int[leftArrarySize];            
+            for (int index = 0; index < leftArrarySize; index++)
+                leftArray[index] = dataStream[left + index];
+
+            int[] rightArray = new int[rightArrarySize];
+            for (int index = 0; index < rightArrarySize; index++)
+                rightArray[index] = dataStream[middle+1 + index];
+
+            int leftArrayIndex = 0, rightArrayIndex = 0;
+
+            int i = left;
+            while (leftArrayIndex < leftArrarySize && rightArrayIndex < rightArrarySize)
+            {
+                if (leftArray[leftArrayIndex] < rightArray[rightArrayIndex])
+                {
+                    dataStream[i] = leftArray[leftArrayIndex++];
+                }
+                else
+                {
+                    dataStream[i] = rightArray[rightArrayIndex++];
+                }
+                i++;
+            }
+            while(leftArrayIndex<leftArrarySize)
+                dataStream[i++] = leftArray[leftArrayIndex++];
+            while (rightArrayIndex < rightArrarySize)
+                dataStream[i++] = rightArray[rightArrayIndex++];
+        }
+        public static void PrintArray(int[] dataStream, string message)
+        {
+            Console.WriteLine(message);
+            for (int i = 0; i < dataStream.Length; i++)
+                Console.Write($"{dataStream[i]} ");
+            Console.WriteLine("");
+        }
+    }
+}
--- src/algo/sort/ReLearn.Alog.Sort/Program.cs
+++ src/algo/sort/ReLearn.Alog.Sort/Program.cs
@@ -8,7 +8,8 @@ namespace ReLearn.Alog.Sort
         {
             //SelectionSort.Sort(new int[] { 64, 25, 12, 22, 11 });
             //InsertionSort.Sort(new int[] { 9, 4, 5, 1, 3, 2 });
-           QuickSort.Sort(new int[] { 9, 4, 5, 1, 3, 2 });
+            //QuickSort.Sort(new int[] { 9, 4, 5, 1, 3, 2 });
+            MergeSort.Sort(new int[] { 9, 4, 5, 1, 3, 2 });
             Console.Read();
         }
     }
