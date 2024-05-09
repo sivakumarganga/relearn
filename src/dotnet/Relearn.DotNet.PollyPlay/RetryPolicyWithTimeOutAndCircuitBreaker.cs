a422c07be4a00c16c46ce76a146db89fbf09c1cd
Sivakumar Siddam <sivakumarganga@gmail.com>
2024-05-09 19:35:59 +0530

    Timeout strategy

--- src/dotnet/Relearn.DotNet.PollyPlay/RetryPolicyWithTimeOutAndCircuitBreaker.cs
+++ src/dotnet/Relearn.DotNet.PollyPlay/RetryPolicyWithTimeOutAndCircuitBreaker.cs
@@ -9,23 +9,28 @@ namespace Relearn.DotNet.PollyPlay
             Console.WriteLine("########## Retry Policy with TimeOut and Circuit Breaker ##########");
             //This policy will retry 10 times with 2 seconds delay
             var retryPolicy = Policy.Handle<Exception>()
-                .WaitAndRetryAsync(10, retryAttempt => TimeSpan.FromSeconds(2), (exception, timeSpan, retryCount, context) =>
+                .WaitAndRetryAsync(10, retryAttempt => TimeSpan.FromSeconds(1), (exception, timeSpan, retryCount, context) =>
                 {
-                    Console.WriteLine($"Retry count {DateTime.Now.ToShortTimeString()}:({exception.GetType().Name}): #{retryCount} Message:{exception.Message}");
+                    Console.WriteLine($"Retry count #{retryCount} {DateTime.Now.ToShortTimeString()}:({exception.GetType().Name}):  Message:{exception.Message}");
                 });
             // This policy will open the circuit if 2 exceptions occur and blocks all the retries for 10 seconds
             var circuitBreakerPolicy = Policy.Handle<Exception>()
-                .CircuitBreakerAsync(2, TimeSpan.FromSeconds(10), (exception, timeSpan, context) =>
+                .CircuitBreakerAsync(3, TimeSpan.FromSeconds(10), (exception, timeSpan, context) =>
                 {
                     Console.WriteLine($"Circuit Breaker Opened: {DateTime.Now.ToShortTimeString()}");
                 }, (context) =>
                 {
                     Console.WriteLine("Circuit Breaker Reset");
+                }, () =>
+                {
+                    Console.WriteLine("Circuit Breaker Half Open");
+
                 });
 
-            var timeoutPolicy = Policy.TimeoutAsync(5, (context, timeSpan, task) =>
+            var timeoutPolicy = Policy.TimeoutAsync(1, Polly.Timeout.TimeoutStrategy.Pessimistic, (context, timeSpan, task) =>
             {
-                Console.WriteLine($" ******** Timeout Exception: {timeSpan}");
+                Console.WriteLine($" ******** Timeout Policy: {timeSpan}");
+                
                 return task;
             });
 
@@ -37,27 +42,25 @@ namespace Relearn.DotNet.PollyPlay
             // Each policy maintains its own retry count
             await policy.ExecuteAsync(async () =>
             {
-               
-                await Task.CompletedTask;
 
                 try
                 {
                     Console.WriteLine($"Executing the task Count:{count} at: {DateTime.Now}");
-                    if (count < 10)
+                    if (count < 3)
                     {
-                        Task.Delay(1000).Wait();
+                        Task.Delay(0).Wait();
                         throw new Exception($"This is exception message :{count}");
                     }
-                       
 
-                    if (count == 10)
+
+                    if (count ==3)
                     {
-                        Task.Delay(20000).Wait();
-                       // throw new TimeoutException($"This is time out exception message:{count}");
+                        await Task.Delay(5000);
+                        throw new TimeoutException($"This is time out exception message:{count}");
                     }
-                        
 
-                    if (count == 11)
+
+                    if (count > 3)
                         return;
                 }
                 catch (Exception ex)
